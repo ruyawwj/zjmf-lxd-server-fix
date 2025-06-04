8e006384703d4f2ca1d63effca214d25482e30a4
BulaSete <36999228+xkatld@users.noreply.github.com>
2025-06-04 18:44:42 +0800

    Update config_handler.py

--- config_handler.py
+++ config_handler.py
@@ -20,11 +20,14 @@ class AppConfig:
         self.network_bridge = parser.get('lxc', 'NETWORK_BRIDGE', fallback='lxdbr0')
         self.storage_pool = parser.get('lxc', 'STORAGE_POOL', fallback='default')
         self.default_container_user = parser.get('lxc', 'DEFAULT_CONTAINER_USER', fallback='root')
-
+        self.main_interface = parser.get('lxc', 'MAIN_INTERFACE', fallback=None)
         self.nat_listen_ip = parser.get('lxc', 'NAT_LISTEN_IP', fallback=None)
 
         if not self.nat_listen_ip:
             raise ValueError("配置文件 [lxc] 中必须设置 NAT_LISTEN_IP，因为NAT模式已固定开启")
+        
+        if not self.main_interface:
+            raise ValueError("配置文件 [lxc] 中必须设置 MAIN_INTERFACE (主网卡名)，用于iptables MASQUERADE规则")
 
 
         self.managed_vhost_dir = parser.get('apache', 'MANAGED_VHOST_DIR', fallback=None)
