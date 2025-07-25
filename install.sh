708a954d8589a56d8a6e826e21f418e71be5c4d2
欢 <2711452309@qq.com>
2025-07-26 04:01:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ SCRIPTS=(
     ["7"]="查看镜像列表;https://raw.githubusercontent.com/StarVM-OpenSource/zjmf-lxd-server-fix/refs/heads/main/shell/7.sh"
     ["8"]="查看被控管理网页登录信息;https://raw.githubusercontent.com/StarVM-OpenSource/zjmf-lxd-server-fix/refs/heads/main/shell/8.sh"
     ["9"]="获取魔方对接端口与对接密钥;https://raw.githubusercontent.com/StarVM-OpenSource/zjmf-lxd-server-fix/refs/heads/main/shell/9.sh"
+    ["10"]="设置未传递镜像时的默认镜像;https://raw.githubusercontent.com/StarVM-OpenSource/zjmf-lxd-server-fix/refs/heads/main/shell/10.sh"
 )
 
 # --- 颜色定义 ---
