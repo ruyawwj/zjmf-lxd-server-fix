cf6984575605ad92e16e5f0c3c83365fc8216c17
欢 <2711452309@qq.com>
2025-07-27 02:50:45 +0800

    Update 7.sh

--- shell/7.sh
+++ shell/7.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# 确保 snap 的 lxc 可用
+if ! command -v lxc >/dev/null 2>&1; then
+  echo 'alias lxc="/snap/bin/lxc"' >> /root/.bashrc
+  export PATH=$PATH:/snap/bin
+  source /root/.bashrc
+fi
+
 #列出镜像列表
 lxc image list
